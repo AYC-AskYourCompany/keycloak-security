2e3ef4efa343b3bf214ba3f7f5b84c394bb1cc1a
msauter@proqrent.de <Bernhard123>
2020-12-15 17:35:22 +0100

    initial commit

---
+++ src/main/java/com/ayc/keycloaksecurity/KeycloakSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.ayc.keycloaksecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakSecurityApplication.class, args);
+	}
+
+}
